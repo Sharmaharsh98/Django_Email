7bc4447d9bf6f765caa83db30e7bcadede66012d
harshsharma98 <harshsharma0625@gmail.com>
2023-03-17 10:41:05 +0530

    secret key remove

--- Send_email/Send_email/settings.py
+++ Send_email/Send_email/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9@do95mz(1sc@@_bfj_k)u2p8_!*5au1@b-q22fcpw%_y%(6=8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
